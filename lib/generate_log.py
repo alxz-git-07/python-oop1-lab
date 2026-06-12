aa32f1df5d5d6f3b2d50cccfb61b80c84eac1025
Alexander Kimani <alexanderkimani.07@gmail.com>
2026-06-12 09:44:25 +0300

    bug fix

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -1,14 +1,15 @@
 from datetime import datetime
 import requests
 
-log_data=['User logged in','User updated profile','Report exported']
-filename=f'log_{datetime.now().strftime('%Y%m%d')}.txt'
+def generate_log():
+    log_data=['User logged in','User updated profile','Report exported']
+    filename=f'log_{datetime.now().strftime('%Y%m%d')}.txt'
 
-with open(filename,'w') as file:
-    for entry in log_data:
+    with open(filename,'w') as file:
+      for entry in log_data:
         file.write(f'{entry}\n')
 
-print(f'Log written to {filename}')
+    print(f'Log written to {filename}')
 
 
 def fetch_data():
